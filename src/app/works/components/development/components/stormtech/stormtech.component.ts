7d70fdacdd9f9a7c3eac5972551c742cfd14637c
SBellido <sebastianbellidodg@gmail.com>
2023-07-14 18:35:40 +0200

    Stormtech component is working ok

--- src/app/works/components/development/components/stormtech/stormtech.component.ts
+++ src/app/works/components/development/components/stormtech/stormtech.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Button, GalleryImage } from 'src/app/models/models';
 
 @Component({
   selector: 'app-stormtech',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StormtechComponent implements OnInit {
 
-  constructor() { }
+ 
+  currentImageNumber: number = 1;
+  public area: string = 'desarrollo';
+  public icon: string = 'developer_mode';
+  imgGallery: string = '../assets/images/dev/stormtech/desktop/0.png';
+
+  images : GalleryImage[] = [
+    { src: '../assets/images/dev/stormtech/desktop/0.png', position: 0, alt: 'string', first: true, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/1.png', position: 1, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/2.png', position: 2, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/3.png', position: 3, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/4.png', position: 4, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/5.png', position: 5, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/6.png', position: 6, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/8.png', position: 7, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/7.png', position: 8, alt: 'string', first: false, last: false },
+    { src: '../assets/images/dev/stormtech/desktop/9.png', position: 9, alt: 'string', first: false, last: true }
+  ] 
+
+  brands : Button[] = [
+    {
+      route: 'works/development/hunt',
+      img: '../assets/images/dev/hunt/hunt_logo.png',
+      title: 'Hunt',
+      text: 'Logo de Hunt',
+    }
+  ]
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.topFunction();
+    // this.updateButtonStatus();
   }
+  
+/* ----------- MÉTODOS ----------- */
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  routeBack() {    
+    this.router.navigateByUrl('works/design/ux-ui')
+  }
+  routeTo(route:string) {    
+    this.router.navigateByUrl(route);
+  }
+  
 
 }
+
