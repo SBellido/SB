98c27547b4cc63ff150c56d9ed766cc329be4cf3
SBellido <sebastianbellidodg@gmail.com>
2023-07-25 17:41:51 +0200

    Gallery Desktop working

--- src/app/works/components/design/components/ux-ui/components/ypf/ypf.component.ts
+++ src/app/works/components/design/components/ux-ui/components/ypf/ypf.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Button, GalleryImage } from 'src/app/models/models';
+import { GalleryDesptopComponent } from '../../../../../gallery-desptop/gallery-desptop.component';
 
 @Component({
   selector: 'app-ypf',
@@ -8,29 +9,18 @@ import { Button, GalleryImage } from 'src/app/models/models';
   styleUrls: ['./ypf.component.scss']
 })
 export class YpfComponent implements OnInit {
-
-  currentImageNumber: number = 1;
+  
   public list: string = 'list';
   public area: string = 'diseño/ux|ui';
   public icon: string = 'local_florist';
-  selectedOption: string = '';
+
   imgGallery: string = '../assets/images/ux_ui/ypf/asignarComite/0.png';
+  
+  currentImageIndex: number = 0;
+  imagesCount: number = 0;
+  imagesList : GalleryImage[] = [];
 
-  imagesList : GalleryImage[] = [  ];
-  imagesList2 : GalleryImage[] = [
-    // { src: '../assets/images/ux_ui/ypf/asignarComite/0.png', position: 0, alt: 'string', first: true, last: false },
-    //   { src: '../assets/images/ux_ui/ypf/asignarComite/1.png', position: 1, alt: 'string', first: false, last: false },
-    //   { src: '../assets/images/ux_ui/ypf/asignarComite/2.png', position: 2, alt: 'string', first: false, last: false },
-    //   { src: '../assets/images/ux_ui/ypf/asignarComite/3.png', position: 3, alt: 'string', first: false, last: true }
-
-  ];
   brands : Button[] = [
-    // {
-    //   route: 'works/design/ux-ui/ypf',
-    //   img: '../assets/images/ux_ui/ypf/YPF_logo.png',
-    //   title: 'YPF',
-    //   text: 'Logo de YPF',
-    // },
     {
       route: 'works/design/ux-ui/v-life',
       img: '../assets/images/ux_ui/v-life/logo/v_life_isologo.png',
@@ -55,58 +45,10 @@ export class YpfComponent implements OnInit {
 
   ngOnInit(): void {
     this.topFunction();
-    // this.updateButtonStatus();
+    this.loadImages(); 
   }
   
 /* ----------- MÉTODOS ----------- */
-
-onChange(event: Event) {
-  alert(this.imagesList[1]);
-  const selectedValue = (event.target as HTMLSelectElement).value;
-  if (selectedValue === 'imagesList') {
-    this.imagesList = [
-      { src: '../assets/images/ux_ui/ypf/asignarComite/0.png', position: 0, alt: 'string', first: true, last: false },
-      { src: '../assets/images/ux_ui/ypf/asignarComite/1.png', position: 1, alt: 'string', first: false, last: false },
-      { src: '../assets/images/ux_ui/ypf/asignarComite/2.png', position: 2, alt: 'string', first: false, last: false },
-      { src: '../assets/images/ux_ui/ypf/asignarComite/3.png', position: 3, alt: 'string', first: false, last: true }
-    ];
-    this.imgGallery = '../assets/images/ux_ui/ypf/asignarComite/0.png';
-    alert(this.imagesList[1]);
-
-  } else if (selectedValue === 'imagesList2') {
-    this.imagesList2 = [
-      { src: '../assets/images/ux_ui/ypf/asignarComite/2.png', position: 0, alt: 'string', first: true, last: false },
-      { src: '../assets/images/yo.png', position: 1, alt: 'string', first: false, last: true }
-    ];
-    this.imgGallery = '../assets/images/azulejo.png';
-  }
-}
-
-
-selectOption(option: string) {
-  this.selectedOption = option;
-  // Realiza las acciones necesarias según la opción seleccionada
-  if (option === 'option1') {
-    // Cambiar la variable imgGallery y el arreglo de imágenes según la opción 1
-    this.imgGallery = '../assets/images/opcion1.png';
-    this.imagesList = [
-      // Nuevas imágenes para la opción 1
-    ];
-  } else if (option === 'option2') {
-    // Cambiar la variable imgGallery y el arreglo de imágenes según la opción 2
-    this.imgGallery = '../assets/images/opcion2.png';
-    this.imagesList = [
-      // Nuevas imágenes para la opción 2
-    ];
-  } else if (option === 'option3') {
-    // Cambiar la variable imgGallery y el arreglo de imágenes según la opción 3
-    this.imgGallery = '../assets/images/opcion3.png';
-    this.imagesList = [
-      // Nuevas imágenes para la opción 3
-    ];
-  }
-}
-
   topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
@@ -118,8 +60,46 @@ selectOption(option: string) {
     this.router.navigateByUrl(route);
   }
 
+  onChange(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    if (selectedValue === 'imagesList') {
+      this.loadImages();
+    } else if (selectedValue === 'imagesList2') {
+      this.loadImages2();
+    } else if (selectedValue === 'imagesList3') {
+      this.loadImages3();
+    }
+  }
 
-  
+  loadImages() {
+    this.imagesList = [
+      { src: '../assets/images/ux_ui/ypf/asignarComite/0.png', position: 0, alt: 'string', first: true, last: false },
+      { src: '../assets/images/ux_ui/ypf/asignarComite/1.png', position: 1, alt: 'string', first: false, last: false },
+      { src: '../assets/images/ux_ui/ypf/asignarComite/2.png', position: 2, alt: 'string', first: false, last: false },
+      { src: '../assets/images/ux_ui/ypf/asignarComite/3.png', position: 3, alt: 'string', first: false, last: true }
+    ];
+    this.imgGallery = this.imagesList[0].src;
+  }
+  loadImages2() {
+    this.imagesList = [
+      { src: '../assets/images/ux_ui/ypf/asignarComite/2.png', position: 0, alt: 'string', first: true, last: false },
+      { src: '../assets/images/yo.png', position: 1, alt: 'string', first: false, last: true }
+    ];
+    this.imgGallery = this.imagesList[0].src;
+  }
+  loadImages3() {
+    this.imagesList = [
+      { src: '../assets/images/firma_sb.svg', position: 0, alt: 'string', first: true, last: false },
+      { src: '../assets/images/logo_eye.svg', position: 1, alt: 'string', first: false, last: true }
+    ];
+    this.imgGallery = this.imagesList[0].src; // Asigna la primera imagen del arreglo
+  }
+
+  updateCounter() {
+    // Actualizar el contador con la cantidad de imágenes cargadas
+    this.imagesCount = this.imagesList.length;
+  }
+
 
 }
 
