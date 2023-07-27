5d76c328e9a6cce8f295359e3028df4949fbcf3e
SBellido <sebastianbellidodg@gmail.com>
2023-07-27 19:59:32 +0200

    All principal sections display good

--- src/app/global/components/footer/footer.component.ts
+++ src/app/global/components/footer/footer.component.ts
@@ -26,19 +26,19 @@ export class FooterComponent implements OnInit {
     {  
       route: 'https://www.linkedin.com/in/sebastian-bellido-9a0a6681/', 
       img: '../../../../assets/images/social_network/linkeding/logo_social_network_linkedin.svg',
-      title: 'Sebastián Bellido en Linkedin', 
+      title: 'Sebastián en Linkedin', 
       text:'logo de linkedin' 
     },
     {  
       route: 'https://www.behance.net/sebastianba389', 
       img: '../../../../assets/images/social_network/behance/logo_social_network_behance.svg',
-      title: 'Sebastián Bellido en Behance', 
+      title: 'Sebastián en Behance', 
       text:'logo de Behance' 
     },
     {  
       route: 'https://github.com/SBellido', 
       img: '../../../../assets/images/social_network/github/logo_social_network_github.svg',
-      title: 'Sebastián Bellido en Github', 
+      title: 'Sebastián en Github', 
       text:'logo de github' 
     },
   ]
@@ -46,14 +46,14 @@ export class FooterComponent implements OnInit {
     {  
       route: 'mailto:sebastianbellidodg@gmail.com?subject=Hola Seba!&body=¿Qué propones?.', 
       img: 'alternate_email',
-      title: 'Enviar email', 
-      text: 'Enviar email' 
+      title: 'Envía un email', 
+      text: 'Email' 
     },
     {  
       route: 'https://wa.me/541121580848', 
       img: 'perm_phone_msg',
-      title: 'Conectar WhatsApp', 
-      text: 'Conectar WhatsApp' 
+      title: 'Usa WhatsApp', 
+      text: '* WhatsApp' 
     },
   ]
 
