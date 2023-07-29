fc1dc4b0258ad3c55cdb7ece17a4b2a25756bec8
SBellido <sebastianbellidodg@gmail.com>
2023-07-30 00:38:21 +0200

    Working in galleries polaroid

--- src/app/works/components/design/components/brands-section/brands-section.component.ts
+++ src/app/works/components/design/components/brands-section/brands-section.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Button, GalleryImage } from 'src/app/models/models';
+import { GalleryComponent } from '../../../gallery/gallery.component';
 
 @Component({
   selector: 'app-brands-section',
@@ -10,47 +11,80 @@ import { Button, GalleryImage } from 'src/app/models/models';
 
 export class BrandsSectionComponent implements OnInit {
 
+  @ViewChild('galleryComponent') galleryComponent!: GalleryComponent;
 
- /*--------VARIABLES -------*/ 
- public area: string = 'diseño/marcas';
- public icon: string = 'local_florist';
-//  public isMobile: boolean = false;
+  /*--------VARIABLES -------*/ 
+  public area: string = 'diseño/gráfico';
+  public icon: string = 'local_florist';
 
- imgGallery = '../assets/images/brands/0.png';
- imagesList : GalleryImage[] = [
-   { src: '../assets/images/brands/0.png', position: 0, alt: 'string', first: true, last: false },
-   { src: '../assets/images/brands/1.png', position: 1, alt: 'string', first: false, last: false },
-   { src: '../assets/images/brands/2.png', position: 2, alt: 'string', first: false, last: false },
-   { src: '../assets/images/brands/4.png', position: 3, alt: 'string', first: false, last: true },
- ] 
+  imgGallery = '../assets/images/brands/0.png';
+  imagesList : GalleryImage[] = [
+    { src: '../assets/images/brands/0.png', position: 0, alt: 'string', first: true, last: false },
+    { src: '../assets/images/brands/1.png', position: 1, alt: 'string', first: false, last: false },
+    { src: '../assets/images/brands/2.png', position: 2, alt: 'string', first: false, last: false },
+    { src: '../assets/images/brands/3.png', position: 3, alt: 'string', first: false, last: false },
+    { src: '../assets/images/brands/4.png', position: 4, alt: 'string', first: false, last: false },
+    { src: '../assets/images/brands/5.png', position: 5, alt: 'string', first: false, last: false },
+    { src: '../assets/images/brands/6.png', position: 6, alt: 'string', first: false, last: false },
+    { src: '../assets/images/brands/7.png', position: 7, alt: 'string', first: false, last: false },
+    { src: '../assets/images/brands/8.png', position: 8, alt: 'string', first: false, last: true }
+  ] 
 
-brands: Button[] = [
-  {
-    route: 'works/design/competitions',
-    img: '../assets/images/graph/eyeka_logo.png',
-    title: 'Stormtech',
-    text: 'Logo de Stormtech',
-  },
-]
+  brands : Button[] = [
+    {
+      route: 'works/design/competitions',
+      img: '../assets/images/graph/eyeka_logo.png',
+      title: 'Marcas',
+      text: 'Logo de Marca Registrada',
+    },
+  ]
  
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.topFunction();
+    this.loadImages(); 
   }
-   
- /* ----------- MÉTODOS ----------- */
- topFunction() {
-   document.body.scrollTop = 0;
-   document.documentElement.scrollTop = 0;
- }
- 
- routeBack() {    
-   this.router.navigateByUrl('works/design/ux-ui')
- }
+  
+/* ----------- MÉTODOS ----------- */
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  routeBack() {    
+    this.router.navigateByUrl('works/design/ux-ui')
+  }
+  routeTo(route:string) {    
+    this.router.navigateByUrl(route);
+  }
 
- 
+  loadImages() {
+    this.imagesList = [
+      
+    ];
+    this.imgGallery = this.imagesList[0].src;
+  }
+  loadImages2() {
+    this.imagesList = [
+      { src: '../assets/images/competitions/doritos/0.png', position: 0, alt: 'string', first: true, last: false },
+      { src: '../assets/images/competitions/doritos/1.png', position: 1, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/2.png', position: 2, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/3.png', position: 3, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/4.png', position: 4, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/5.png', position: 5, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/6.png', position: 6, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/7.png', position: 7, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/8.png', position: 8, alt: 'string', first: false, last: false },
+      { src: '../assets/images/competitions/doritos/9.png', position: 9, alt: 'string', first: false, last: true },
+    ];
+    this.imgGallery = this.imagesList[0].src;
+  }
+  loadImages3() {
+    this.imagesList = [
+      { src: '../assets/images/competitions/sweetart/0.png', position: 0, alt: 'string', first: true, last: false },   
+      { src: '../assets/images/competitions/sweetart/1.png', position: 1, alt: 'string', first: false, last: true },
+    ];
+    this.imgGallery = this.imagesList[0].src;
+  }
 
 }
-
-
