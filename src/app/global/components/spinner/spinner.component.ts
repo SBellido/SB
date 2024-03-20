54f4fe17b94bc5d3ad2708ad6e3a30f7dd5e7ad3
SBellido <sebastianbellidodg@gmail.com>
2024-03-20 13:53:33 -0300

    Spinner was added

--- src/app/global/components/spinner/spinner.component.ts
+++ src/app/global/components/spinner/spinner.component.ts
@@ -11,6 +11,12 @@ export class LoaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // Espera máximo 2 segundos para que se complete la carga
+    setTimeout(() => {
+      this.isLoading = false; // Después de 2 segundos, establece isLoading a false para ocultar el spinner
+    }, 1500);
+    
+    // Listener para verificar si la carga se completó antes de los 2 segundos
     window.addEventListener('load', () => {
       this.isLoading = false; // Después de cargar completamente la página, establece isLoading a false para ocultar el spinner
     });
