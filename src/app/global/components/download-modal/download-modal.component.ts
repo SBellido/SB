fe846c2a9d2f9230ff367c6e7fe9107c5ccc1638
SBellido <sebastianbellidodg@gmail.com>
2024-04-18 18:07:34 -0300

    File´s name spanish cv was corrected

--- src/app/global/components/download-modal/download-modal.component.ts
+++ src/app/global/components/download-modal/download-modal.component.ts
@@ -1,22 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { MatDialogRef } from '@angular/material/dialog';
-
-// @Component({
-//   selector: 'app-download-modal',
-//   templateUrl: './download-modal.component.html',
-//   styleUrls: ['./download-modal.component.scss']
-// })
-// export class DownloadModalComponent implements OnInit {
-
-//   constructor(private dialogRef: MatDialogRef<DownloadModalComponent>) { }
-//   ngOnInit(): void {
-//   }
-
-//   closeModal() {
-//     this.dialogRef.close();
-//   }
-// }
-// download-modal.component.ts
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { MatDialogRef } from '@angular/material/dialog';
